c52f9e1759a7354917fcf6c8e6f2516572150826
Biswarup35 <biswarup35@gmail.com>
2022-01-24 18:39:44 +0530

    [typo] deleting typos

--- src/views/Criptocurrencies.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-interface CriptocurrenciesProps {}
-
-const Criptocurrencies: React.FunctionComponent<CriptocurrenciesProps> = () => {
-  return <h1>Criptocurrency page</h1>;
-};
-
-export default Criptocurrencies;
