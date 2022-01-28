edc954ce1240e7a0f7b71f9d7b336441627cf919
Biswarup35 <biswarup35@gmail.com>
2022-01-28 19:30:45 +0530

    [update] keys are updated

--- src/services/criptoApi.ts
+++ src/services/criptoApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
   "x-rapidapi-host": process.env.REACT_APP_CRYPTO_API_HOST,
-  "x-rapidapi-key": process.env.REACT_APP_CRYPTO_API_KEY,
+  "x-rapidapi-key": process.env.REACT_APP_API_KEY,
 };
 const baseUrl = process.env.REACT_APP_CRYPTO_API_BASE_URL;
 const createRequest = (url: any) => ({ url, headers: cryptoApiHeaders });
--- src/services/newsApi.ts
+++ src/services/newsApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const newsApiHeaders = {
   "x-bingapis-sdk": "true",
   "x-rapidapi-host": process.env.REACT_APP_NEWS_API_HOST,
-  "x-rapidapi-key": process.env.REACT_APP_NEWS_API_KEY,
+  "x-rapidapi-key": process.env.REACT_APP_API_KEY,
 };
 
 const baseUrl = process.env.REACT_APP_NEWS_API_BASE_URL;
